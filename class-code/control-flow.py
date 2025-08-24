e4cd45786e0a499ebdbefe13af4d3860444f1ad0
omarakamal <omaraakamal@gmail.com>
2025-08-24 11:21:36 +0300

    if statements

--- class-code/control-flow.py
+++ class-code/control-flow.py
@@ -9,5 +9,23 @@ print(7 != 7)
 
 print(9 > 3 and 5 < 6)
 
+# ! == not
+# || == or
+# && == and
 print( not 5 > 4)
+
+
+
+# if Statements in Python
+my_teacher = input("Enter Teacher Name: ").lower()
+
+if my_teacher == "omar":
+    print("You are teaching Unit 2 and 4")
+elif my_teacher == "george":
+    print("You are teaching Unit 1 and 3")
+else:
+    print("You dont have to teach any Units :)")
+
+print("Something")
+
 
