f033f8aef7a01a564ffd1d641f3f59bf3938e381
omarakamal <omaraakamal@gmail.com>
2025-08-24 10:05:52 +0300

    data types

--- class-code/intro-to-python.py
+++ class-code/intro-to-python.py
@@ -16,3 +16,25 @@ my_student= 'Mohammad'
 MY_FAVORITE_NUMBER = 5
 print(MY_FAVORITE_NUMBER)
 
+
+
+
+# Data types in Python
+
+# str
+print(type("Ali"))
+
+# int
+print(type(5))
+
+# float
+print(type(5.))
+
+
+# Boolean
+print(True)
+print(False)
+
+# || &&, and or
+print(True and False)
+print(True or False)
